7a79ba84072f13dfd4b85bc5b2cee1231a3ef76a
JaeSang Yoo <jsyoo5b@gmail.com>
2023-11-28 03:31:22 +0900

    refactor: Optimize ImageBlock marshaling with embedding

--- kakaowork/ImageBlock.go
+++ kakaowork/ImageBlock.go
@@ -5,7 +5,7 @@ import "encoding/json"
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/imagelinkblock/
 
 type ImageBlock struct {
-	Url string `validate:"required,url"`
+	Url string `json:"url" validate:"required,url"`
 }
 
 func (i ImageBlock) Type() string {
@@ -17,13 +17,12 @@ func (i ImageBlock) String() string {
 }
 
 func (i ImageBlock) MarshalJSON() ([]byte, error) {
-	type _ImageLink struct {
+	type Embed ImageBlock
+	return json.Marshal(&struct {
 		Type string `json:"type"`
-		Url  string `json:"url"`
-	}
-
-	return json.Marshal(_ImageLink{
-		Type: i.Type(),
-		Url:  i.Url,
+		Embed
+	}{
+		Type:  i.Type(),
+		Embed: (Embed)(i),
 	})
 }
