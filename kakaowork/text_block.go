526b8de92e0bece7c6a7cffef2cd9666708cf972
JaeSang Yoo <jsyoo5b@gmail.com>
2024-01-22 23:06:42 +0900

    docs: add documentation for TextBlock and relates

--- kakaowork/text_block.go
+++ kakaowork/text_block.go
@@ -5,17 +5,18 @@ import (
 	"strings"
 )
 
+// TextBlock is a BubbleBlock to represent simple texts.
+// When Text and Inlines differ, Inlines applied prior.
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/textblock/
-
 type TextBlock struct {
-	Text    string   `json:"text"`
+	// Text represents plain text without formatting
+	Text string `json:"text"`
+	// Inlines are partial blocks for text with formatting
+	// See also InlineStyled, InlineLink, InlineMention and Inline
 	Inlines []Inline `json:"inlines,omitempty"`
 }
 
-func (t TextBlock) Type() string {
-	return "text"
-}
-
+func (t TextBlock) Type() string { return "text" }
 func (t TextBlock) String() string {
 	if t.Inlines == nil {
 		return t.Text
@@ -27,7 +28,6 @@ func (t TextBlock) String() string {
 		return strings.Join(inlineTexts, "")
 	}
 }
-
 func (t TextBlock) MarshalJSON() ([]byte, error) {
 	type Embed TextBlock
 	return json.Marshal(&struct {
--- kakaowork/text_inline.go
+++ kakaowork/text_inline.go
@@ -2,21 +2,28 @@ package kakaowork
 
 import "encoding/json"
 
+// Inline is type casting interface for TextBlock.Inlines.
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/textblock/#inlines
-
 type Inline interface {
-	BubbleBlock
 	InlineType() string
+	BubbleBlock
 }
 
+// InlineStyled gives styles to text.
+// Style are represented by each field, and it can be applied at the same time.
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/textblock/#styled
-
 type InlineStyled struct {
-	Text   string      `json:"text"`
-	Bold   bool        `json:"bold,omitempty"`
-	Italic bool        `json:"italic,omitempty"`
-	Strike bool        `json:"strike,omitempty"`
-	Color  InlineColor `json:"color,omitempty"`
+	// Text to apply styles
+	Text string `json:"text"`
+	// Bold style, disabled by default
+	Bold bool `json:"bold,omitempty"`
+	// Italic style, disabled by default
+	Italic bool `json:"italic,omitempty"`
+	// Strike style, disabled by default
+	Strike bool `json:"strike,omitempty"`
+	// Color supports InlineColorRed, InlineColorBlue, InlineColorGrey.
+	// InlineColorDefault of empty applies default plain text style
+	Color InlineColor `json:"color,omitempty"`
 }
 
 type InlineColor string
@@ -37,14 +44,9 @@ var inlineColorConstants = map[InlineColor]bool{
 	InlineColorGrey:    true,
 }
 
-func (i InlineStyled) Type() string {
-	return "inline"
-}
-
-func (i InlineStyled) String() string {
-	return i.Text
-}
-
+func (i InlineStyled) InlineType() string { return "styled" }
+func (i InlineStyled) Type() string       { return "inline" }
+func (i InlineStyled) String() string     { return i.Text }
 func (i InlineStyled) MarshalJSON() ([]byte, error) {
 	if _, exists := inlineColorConstants[i.Color]; !exists {
 		i.Color = InlineColorDefault
@@ -60,24 +62,20 @@ func (i InlineStyled) MarshalJSON() ([]byte, error) {
 	})
 }
 
-func (i InlineStyled) InlineType() string {
-	return "styled"
-}
-
+// InlineLink links text to scheme (http/https, mailto, tel).
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/textblock/#link
-
 type InlineLink struct {
+	// Text to apply link
 	Text string `json:"text"`
-	Url  string `json:"url"`
+	// Url starts with "http:", "https:" will link to web browser.
+	// Url starts with "tel:" will link to call.
+	// Url starts with "mailto:" will link to mail client.
+	Url string `json:"url"`
 }
 
-func (i InlineLink) Type() string {
-	return "inline"
-}
-func (i InlineLink) String() string {
-	return i.Text
-}
-
+func (i InlineLink) InlineType() string { return "link" }
+func (i InlineLink) Type() string       { return "inline" }
+func (i InlineLink) String() string     { return i.Text }
 func (i InlineLink) MarshalJSON() ([]byte, error) {
 	type Embed InlineLink
 	return json.Marshal(&struct {
@@ -89,25 +87,18 @@ func (i InlineLink) MarshalJSON() ([]byte, error) {
 	})
 }
 
-func (i InlineLink) InlineType() string {
-	return "link"
-}
-
+// InlineMention can mention the other user.
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/textblock/#mention
-
 type InlineMention struct {
-	Text   string
+	// Text to apply mention
+	Text string
+	// UserId to mention
 	UserId int
 }
 
-func (i InlineMention) Type() string {
-	return "inline"
-}
-
-func (i InlineMention) String() string {
-	return i.Text
-}
-
+func (i InlineMention) InlineType() string { return "mention" }
+func (i InlineMention) Type() string       { return "inline" }
+func (i InlineMention) String() string     { return i.Text }
 func (i InlineMention) MarshalJSON() ([]byte, error) {
 	type Ref struct {
 		Type  string `json:"type"`
@@ -126,8 +117,4 @@ func (i InlineMention) MarshalJSON() ([]byte, error) {
 			Value: i.UserId,
 		},
 	})
-}
-
-func (i InlineMention) InlineType() string {
-	return "mention"
 }
