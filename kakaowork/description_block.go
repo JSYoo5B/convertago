16e7078dd5252cfde22245853521875bfb97b680
JaeSang Yoo <jsyoo5b@gmail.com>
2024-05-03 23:57:28 +0900

    docs: add documentation for Description, Divider and Section Blocks

--- kakaowork/description_block.go
+++ kakaowork/description_block.go
@@ -2,22 +2,20 @@ package kakaowork
 
 import "encoding/json"
 
+// DescriptionBlock 은 말풍선 안에서 짧은 단어와 그에 대한 상세 정보를 설명할 때 사용합니다.
+//
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/descriptionblock/
-
 type DescriptionBlock struct {
+	// Content 에 상세 내용을 TextBlock 으로 입력합니다.
 	Content TextBlock `json:"content"`
-	Term    string    `json:"term"`
-	Accent  bool      `json:"accent,omitempty"`
+	// Term 에 Content 의 내용을 요약하는 문구로 입력합니다.
+	Term string `json:"term"`
+	// Accent 는 Term 의 내용을 굵은 글씨체로 적용하며, 기본값은 false 입니다.
+	Accent bool `json:"accent,omitempty"`
 }
 
-func (d DescriptionBlock) Type() string {
-	return "description"
-}
-
-func (d DescriptionBlock) String() string {
-	return d.Term + ": " + d.Content.String()
-}
-
+func (d DescriptionBlock) Type() string   { return "description" }
+func (d DescriptionBlock) String() string { return d.Term + ": " + d.Content.String() }
 func (d DescriptionBlock) MarshalJSON() ([]byte, error) {
 	type Embed DescriptionBlock
 	return json.Marshal(&struct {
--- kakaowork/section_block.go
+++ kakaowork/section_block.go
@@ -2,22 +2,21 @@ package kakaowork
 
 import "encoding/json"
 
+// SectionBlock 은 TextBlock(좌측)과 ImageBlock(우측)이 조합된 레이아웃 블록으로,
+// 말풍선 안에서 텍스트와 함께 추가 정보를 이미지로 표현하고자 할 때 사용됩니다.
+//
 // Reference: https://docs.kakaoi.ai/kakao_work/blockkit/sectionblock/
-
 type SectionBlock struct {
-	Content   TextBlock    `json:"content"`
-	Accessory *ImageBlock  `json:"accessory,omitempty"`
-	Action    ButtonAction `json:"action,omitempty"`
+	// Content 에 텍스트를 표현하는 속성 블럭을 입력합니다.
+	Content TextBlock `json:"content"`
+	// Accesory 에 이미지를 표현하는 속성 블럭을 입력합니다. (선택)
+	Accessory *ImageBlock `json:"accessory,omitempty"`
+	// ButtonAction 에 클릭 시 수행될 동작을 입력합니다. (선택)
+	Action ButtonAction `json:"action,omitempty"`
 }
 
-func (s SectionBlock) Type() string {
-	return "section"
-}
-
-func (s SectionBlock) String() string {
-	return s.Content.String()
-}
-
+func (s SectionBlock) Type() string   { return "section" }
+func (s SectionBlock) String() string { return s.Content.String() }
 func (s SectionBlock) MarshalJSON() ([]byte, error) {
 	type Embed SectionBlock
 	return json.Marshal(&struct {
